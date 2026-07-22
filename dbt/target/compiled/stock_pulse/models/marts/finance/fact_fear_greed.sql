fb4f0f685b2bc335de47dee82403375b53721b69
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2026-07-22 23:34:24 +0800

    chore: stop tracking dbt target/ dbt/logs and dbt_packages/ (generated files)

--- dbt/target/compiled/stock_pulse/models/marts/finance/fact_fear_greed.sql
+++
@@ -1,9 +0,0 @@
--- dbt/models/marts/finance/fact_fear_greed.sql
-
-
-
-select
-    index_date,
-    fear_greed_score,
-    fear_greed_rating
-from `stock-pulse-260629`.`stockpulse_staging`.`stg_fear_greed`
--- dbt/target/compiled/stock_pulse/models/staging/stg_fear_greed.sql
+++
@@ -1,6 +0,0 @@
--- Fear & Greed 總經指標 staging 層
-select
-    dt as index_date,
-    score as fear_greed_score,
-    fear_greed_rating
-from `stock-pulse-260629`.`stockpulse_staging`.`raw_fear_greed`
